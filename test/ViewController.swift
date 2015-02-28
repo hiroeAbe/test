ba09ca01e669366705c4f625455c5b533b7d87aa
阿部弘枝 <abehiroe@abehiroe-no-MacBook-Air.local>
2015-02-28 10:31:36 +0900

    add new function

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 阿部弘枝 on 2015/02/28.
+//  Copyright (c) 2015年 阿部弘枝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
